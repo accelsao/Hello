65ce0e288e015aed5f105089131802d8a94b982d
ジブリール <jay70550@yahoo.com.tw>
2017-11-30 17:01:44 +0800

    Create Cpp template.cpp

---
+++ Cpp template.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<cstring>
+#include<cstdio>
+#include<queue>
+#include<string>
+#include<map>
+#include<set>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<ctime>
+#include<vector>
+#define Accel ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define LL long long
+#define Re(a,b) memset(a,b,sizeof(a))
+#define N 1100000
+#define M
+#define scanf scanf_s
+using namespace std;
+int main(){
+Accel;
+}
