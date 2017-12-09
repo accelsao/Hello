138987258791a79ba5840e60251c9a7b4b1a7f17
ジブリール <jay70550@yahoo.com.tw>
2017-12-09 10:39:28 +0800

    Update List of algorithms.cpp

--- List of algorithms.cpp
+++ List of algorithms.cpp
@@ -2,13 +2,3 @@
 LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1) % M : pw(a*a%M, b >> 1) : 1; }
 #GCD
 int gcd(int a, int b) { return b ? gcd(b, a%b) : a };
-#BIT
-void add(int x){
-	while (x<N){
-		d[x]++;
-		x += x&(-x);
-	}
-}
-int sum(int x){
-	return x ? d[x] + sum(x - (x&(-x))) : 0;
-}
