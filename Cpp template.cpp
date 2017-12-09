fed84175ae60c595cfaf8987dbf74ffd993009e4
ジブリール <jay70550@yahoo.com.tw>
2017-12-09 10:39:55 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -1,3 +1,4 @@
+#Precode
 #include<iostream>
 #include<cmath>
 #include<algorithm>
@@ -23,3 +24,7 @@ using namespace std;
 int main(){
 Accel;
 }
+#Fast power algorithm
+LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1) % M : pw(a*a%M, b >> 1) : 1; }
+#GCD
+int gcd(int a, int b) { return b ? gcd(b, a%b) : a };
