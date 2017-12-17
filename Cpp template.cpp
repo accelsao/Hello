6b2d29b20e6199b8402726305ab5f5b6e4312f2f
ジブリール <jay70550@yahoo.com.tw>
2017-12-17 08:59:05 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -28,7 +28,7 @@ Accel;
 LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1) % M : pw(a*a%M, b >> 1) : 1; }
 #GCD
 int gcd(int a, int b) { return b ? gcd(b, a%b) : a };
-#EXGCD 求二元一次方程式 -898B - Proper Nutrition-
+#EXGCD
 def ext_euclid(a, b):
     if (b == 0):
         return 1, 0, a
@@ -36,18 +36,4 @@ def ext_euclid(a, b):
         x, y, q = ext_euclid(b, a % b) 
         x, y = y, (x - (a // b) * y)
         return x, y, q
-n=int(input())
-a=int(input())
-b=int(input())
-x,y,d=ext_euclid(a,b)
-if n%d:print('NO')
-else:
-    x*=n//d;y*=n//d
-    a//=d;b//=d
-    if x<0:
-        f=-x//b+(-x%b!=0)
-        x+=f*b;y-=f*a
-    elif y<0:
-        f=-y//a+(-y%a!=0)
-        x-=f*b;y+=f*a
-    print(['NO','YES\n'+str(x)+'  '+str(y)][x*y>=0])
+#ext_euclid(a,b) return x,y,gcd(a,b) ,ax+by=1
