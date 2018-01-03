c191d475344364280a75f07988398e5a9a867418
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-01-03 14:40:40 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -46,10 +46,23 @@ ext_euclid(a,b) return x,y,gcd(a,b) ,ax+by=1
 #define LL long long
 #define Accel ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define Re(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, A, B) for (int I = (A); I <= (B); I++)
+#define REP(I, N) for (int I = 0; I < (N); I++)
+#define MID ((l+r)>>1)
+#define lc (rt<<1)
+#define rc (rt<<1|1)
+#define ls lc,l,MID
+#define rs rc,MID+1,r
+#define st 1,1,n
 using namespace std;
-const int N=210;
-const int M=1e9+7;
 LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1) % M : pw(a*a%M, b >> 1) : 1; }
 LL gcd(LL a, LL b) { return b ? gcd(b, a%b) : a };
+const int N=210;
+const int M=1e9+7;
 ----
 			
