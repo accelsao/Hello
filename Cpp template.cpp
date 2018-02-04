503d2e419bf518c17f83fa243e92f4d27871f7dd
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-02-04 21:41:50 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -53,21 +53,19 @@ ext_euclid(a,b) return x,y,gcd(a,b) ,ax+by=1
 #define ALL(X) (X).begin(), (X).end()
 #define FOR(I, A, B) for (int I = (A); I <= (B); I++)
 #define REP(I, N) for (int I = 0; I < (N); I++)
-#define MID ((l+r)>>1)
-#define lc (rt<<1)
-#define rc (rt<<1|1)
-#define ls lc,l,MID
-#define rs rc,MID+1,r
-#define st 1,1,n
 #define PII pair<int,int>
 #define PLL pair<LL,LL>
 using namespace std;
-const int N=1e3+1;
+const int N=2e5+1;
 const int M=1e9+7;
 const double eps=1e-6;
+typedef complex<double>C;
+const double PI(acos(-1.0));
+//size() use int
 //void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
 //int sum(int x){return x?BIT[x]+sum(x-(x&-x)):0;}
 //int find(int x){return x==p[x]?x:p[x]=find(p[x]);}
 //LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1,M) % M : pw(a*a%M, b>>1,M) : 1; }
+//log() = ln()
 int main(){Accel
 }
