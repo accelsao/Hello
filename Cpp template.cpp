d9d0475fdb236a4e497b3409dd4638e1c695e6ca
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-02-21 09:12:01 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -54,21 +54,24 @@ ext_euclid(a,b) return x,y,gcd(a,b) ,ax+by=1
 #define REP(I, N) for (int I = 0; I < (N); I++)
 #define PII pair<int,int>
 #define PLL pair<LL,LL>
+#define SZ(a) ((int)a.size())
+//#pragma GCC optimize(2)
 using namespace std;
-const int N=2e5+1;
+const int N=1e6+1;
 const int M=1e9+7;
 const double eps=1e-6;
 typedef complex<double>C;
 const double PI(acos(-1.0));
 //size() use int
-//__gcd
+//__gcd, atan2(y,x)=y/x , __int128
 //c(n,k)=c(n-1,k-1)+c(n-1,k)
 //c(i,r),i for r to n =c(n+1,r+1)
 //void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
 //int sum(int x){return x?BIT[x]+sum(x-(x&-x)):0;}
 //int find(int x){return x==p[x]?x:p[x]=find(p[x]);}
 //LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1,M) % M : pw(a*a%M, b>>1,M) : 1; }
-//log() = ln()
+//log() = ln() , log(x)/log(y)=log(y)-base-x
 //watch out LL
+//INT_MAX 127,INT_MIN 128
 int main(){Accel
 }
