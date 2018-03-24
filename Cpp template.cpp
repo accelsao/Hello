8cfac8cc04b465ce1bf2ec93e981b15e37a6e316
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-03-24 11:39:36 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -1,46 +1,3 @@
-#Precode for VS
-#include<iostream>
-#include<cmath>
-#include<algorithm>
-#include<cstring>
-#include<cstdio>
-#include<queue>
-#include<string>
-#include<map>
-#include<set>
-#include<string>
-#include<sstream>
-#include<vector>
-#include<stack>
-#include<ctime>
-#include<vector>
-#define Accel ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define LL long long
-#define Re(a,b) memset(a,b,sizeof(a))
-#define N 1100000
-#define M
-#define scanf scanf_s
-using namespace std;
-int main(){
-Accel;
-}
-----
-#Fast power algorithm
-LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1) % M : pw(a*a%M, b >> 1) : 1; }
-----
-#GCD
-int gcd(int a, int b) { return b ? gcd(b, a%b) : a };
-----
-#EXGCD for py
-def ext_euclid(a, b):
-    if (b == 0):
-        return 1, 0, a
-    else:
-        x, y, q = ext_euclid(b, a % b) 
-        x, y = y, (x - (a // b) * y)
-        return x, y, q
-ext_euclid(a,b) return x,y,gcd(a,b) ,ax+by=1
-----
 #include <bits/stdc++.h>
 #define LL long long
 #define Accel ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -62,8 +19,9 @@ const int M=1e9+7;
 const double eps=1e-6;
 typedef complex<double>C;
 const double PI(acos(-1.0));
-//size() use int
+//for(int mask=i;mask>0;mask=(mask-1)&i)
 //__gcd, atan2(y,x)=y/x , __int128
+//int a[25]= { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 //c(n,k)=c(n-1,k-1)+c(n-1,k)
 //c(i,r),i for r to n =c(n+1,r+1)
 //void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
@@ -71,7 +29,5 @@ const double PI(acos(-1.0));
 //int find(int x){return x==p[x]?x:p[x]=find(p[x]);}
 //LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1,M) % M : pw(a*a%M, b>>1,M) : 1; }
 //log() = ln() , log(x)/log(y)=log(y)-base-x
-//watch out LL
-//INT_MAX 127,INT_MIN 128
-int main(){Accel
-}
+//cout<<fixed<<setprecision(12)<<ans<<endl;
+int main(){Accel}
