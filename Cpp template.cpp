0d5ba31b9e496f3a541dd1e603a671c6e93af019
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-03-29 17:54:28 +0800

    Update Cpp template.cpp

--- Cpp template.cpp
+++ Cpp template.cpp
@@ -20,14 +20,18 @@ const double eps=1e-6;
 typedef complex<double>C;
 const double PI(acos(-1.0));
 //for(int mask=i;mask>0;mask=(mask-1)&i)
-//__gcd, atan2(y,x)=y/x , __int128
 //int a[25]= { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+//__gcd, atan2(y,x)=y/x , __int128
 //c(n,k)=c(n-1,k-1)+c(n-1,k)
 //c(i,r),i for r to n =c(n+1,r+1)
 //void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
 //int sum(int x){return x?BIT[x]+sum(x-(x&-x)):0;}
 //int find(int x){return x==p[x]?x:p[x]=find(p[x]);}
-//LL pw(LL a, LL b,LL M) { return b ? b & 1 ? a*pw(a, b - 1,M) % M : pw(a*a%M, b>>1,M) : 1; }
+//LL pw(LL a,LL b){LL t=1;for(;b;b>>=1,a=a*a%M)b&1?t=t*a%M:0;return t;}
 //log() = ln() , log(x)/log(y)=log(y)-base-x
+//INT_MAX 127,INT_MIN 128
+//int dw[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+//int dw[8][2]={{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
 //cout<<fixed<<setprecision(12)<<ans<<endl;
+//__builtin_popcount(mask)
 int main(){Accel}
