38bd8acdaac02e44fb6d25aec0200de8170d8a17
accelsao <bebe8277@gmail.com>
2019-11-13 12:06:38 +0800

    upload CF 1179C

---
+++ Codeforces/1179C - Serge and Dining Room [segment tree, 2400].cpp
@@ -0,0 +1,123 @@
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏ש‏ש‏שבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏ש‏ש‏ש‏¢iש‏שבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏שבשבשבשבשבשבשבש‏שבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏ש‏שבשבש‏¢i¢i¢i¢i¢iשבשבשבשבשבשב¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏שבשבשבשבשבשבש‏¢i¢iשבשבשבשבשבשבש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבשבשבשבש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏¢iש‏שבשבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏ש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשב¢iש‏שבש‏שבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשב¢iש‏שבשבשבשבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏¢iש‏¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢iשבש‏¢iש‏ש‏שבשבשבש‏ש‏¢iש‏שבשבשבשבשבשב¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבש‏ש‏שבש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבש‏¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשב¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢iש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏שבש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢iש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשב¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢iש‏ש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+#include <bits/stdc++.h>
+using namespace std;
+//#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = (1<<20) + 100;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+int n, m, q;
+int a[N];
+int b[N];
+int tr[N<<2];
+int mx[N<<2];
+int h=1<<20;
+
+void modify(int p, int x){
+	p+=h;
+	tr[p]+=x;
+	mx[p]=tr[p];
+	for(p>>=1;p;p>>=1){
+		tr[p]=tr[p<<1]+tr[p<<1|1];
+		mx[p]=min(mx[p<<1|1], mx[p<<1] + tr[p<<1|1]);
+	}
+}
+
+int query(){
+	int u=1, w=0;
+	while(u<h){
+		if(mx[u<<1|1]<w){
+			u=u<<1|1;
+		}
+		else{
+			w-=tr[u<<1|1];
+			u=u<<1;
+		}
+	}
+	return u==h?-1:u-h;
+}
+
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>n>>m;
+
+	int n0=1<<20;
+	
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		modify(a[i], -1);
+	}
+	for(int i=1;i<=m;i++){
+		cin>>b[i];
+		modify(b[i], 1);
+	}
+	
+	cin>>q;
+	while(q--){
+		int op, i, x;
+		cin>>op>>i>>x;
+		if(op&1){
+			modify(a[i], 1);
+			a[i]=x;
+			modify(a[i], -1);
+		}
+		else{
+			modify(b[i], -1);
+			b[i]=x;
+			modify(b[i], 1);
+		}
+		printf("%d\n", query());
+	}
+	
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+*/
+
+
+
