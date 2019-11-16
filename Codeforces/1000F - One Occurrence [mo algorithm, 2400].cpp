9329e32519f6d82130dbfcc4eba5458d57748308
accelsao <bebe8277@gmail.com>
2019-11-16 11:51:13 +0800

    upload 1000F

---
+++ Codeforces/1000F - One Occurrence [mo algorithm, 2400].cpp
@@ -0,0 +1,122 @@
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏ש‏ש‏שבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏ש‏ש‏ש‏¢iש‏שבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏שבשבשבשבשבשבשבש‏שבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏ש‏שבשבש‏¢i¢i¢i¢i¢iשבשבשבשבשבשב¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏שבשבשבשבשבשבש‏¢i¢iשבשבשבשבשבשבש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבשבשבשבש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏¢iש‏שבשבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏ש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשב¢iש‏שבש‏שבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשב¢iש‏שבשבשבשבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏¢iש‏¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢iשבש‏¢iש‏ש‏שבשבשבש‏ש‏¢iש‏שבשבשבשבשבשב¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבש‏ש‏שבש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבש‏¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשב¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢iש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏שבש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢iש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשב¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢iש‏ש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = 5e5 +10;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+int n, m;
+int a[N];
+int blk[N];
+const int sz=707;
+struct Query{
+	int l, r, id;
+	Query(){}
+	bool operator < (const Query& rhs){
+		return blk[l]==blk[rhs.l] ? (blk[l]&1 ? r>rhs.r: r<rhs.r) : l<rhs.l;
+	}
+}Q[N];
+
+stack<int> s;
+int ins[N], cnt[N], ans[N];
+void update(int i, int op){
+	cnt[a[i]]+=op;
+	if(cnt[a[i]]==1 && !ins[a[i]]){
+		ins[a[i]]=1;
+		s.push(a[i]);
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		blk[i]=i/sz;
+	}
+	cin>>m;
+	for(int i=0;i<m;i++){
+		cin>>Q[i].l>>Q[i].r;
+		Q[i].id=i;
+	}
+	sort(Q, Q+m);
+	int l=1, r=0;
+	for(int i=0;i<m;i++){
+		while(l < Q[i].l){
+			update(l++, -1);
+		}
+		while(l > Q[i].l){
+			update(--l, 1);
+		}
+		while(r < Q[i].r){
+			update(++r, +1);
+		}
+		while(r > Q[i].r){
+			update(r--, -1);
+		}
+		while(SZ(s)&&cnt[s.top()] != 1){
+			ins[s.top()]=0;
+			s.pop();
+		}
+		if(SZ(s)){
+			ans[Q[i].id]=s.top();
+		}
+		else{
+			ans[Q[i].id]=0;
+		}
+	}
+	for(int i=0;i<m;i++)
+		printf("%d\n", ans[i]);
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+*/
+
+
+
