5dcec9684d53d46aec3fd4c473bbf9709c167b8f
accelsao <bebe8277@gmail.com>
2019-11-16 09:11:40 +0800

    upload 1239D and 32E

---
+++ Codeforces/1239D - Catowice City [scc, 2400].cpp
@@ -0,0 +1,139 @@
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏ש‏ש‏שבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏ש‏ש‏ש‏¢iש‏שבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏שבשבשבשבשבשבשבש‏שבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏ש‏שבשבש‏¢i¢i¢i¢i¢iשבשבשבשבשבשב¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏שבשבשבשבשבשבש‏¢i¢iשבשבשבשבשבשבש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבשבשבשבש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏¢iש‏שבשבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏ש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשב¢iש‏שבש‏שבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשב¢iש‏שבשבשבשבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏¢iש‏¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢iשבש‏¢iש‏ש‏שבשבשבש‏ש‏¢iש‏שבשבשבשבשבשב¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבש‏ש‏שבש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבש‏¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשב¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢iש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏שבש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢iש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשב¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢iש‏ש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = 1e6 +10;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+int n, m;
+vector<int> g[N];
+int vis[N], low[N], ins[N], belong[N], dfn, scc;
+
+stack<int> s;
+void tarjan(int u){
+	vis[u]=low[u]=++dfn;
+	ins[u]=1;
+	s.push(u);
+	for(const int& v:g[u]){
+		if(!vis[v]){
+			tarjan(v);
+			low[u]=min(low[u], low[v]);
+		}
+		else if(ins[v]){
+			low[u]=min(low[u], vis[v]);
+		}
+	}
+	if(low[u]==vis[u]){
+		int p;
+		++scc;
+		do{
+			p=s.top();s.pop();
+			ins[p]=0;
+			belong[p]=scc;
+		}while(u!=p);
+	}
+}
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int T;
+	cin>>T;
+	while(T--){
+		cin>>n>>m;
+		for(int i=1;i<=n;i++){
+			g[i].clear();
+			vis[i]=low[i]=ins[i]=belong[i]=0;
+			dfn=scc=0;
+		}
+
+		while(s.size())s.pop();
+		int u, v;
+		for(int i=0;i<m;i++){
+			cin>>u>>v;
+			if(u!=v){
+				g[u].PB(v);
+			}
+		}
+		for(int i=1;i<=n;i++){
+			if(!vis[i]){
+				tarjan(i);
+			}
+		}
+
+		if(scc==1){
+			puts("No");
+		}
+		else{
+			vector<int> a, b;
+			for(int i=1;i<=n;i++){
+				if(belong[i]==1){
+					a.push_back(i);
+				}
+				else{
+					b.push_back(i);
+				}
+			}
+			puts("Yes");
+			printf("%d %d\n", SZ(a), SZ(b));
+			for(const int& x:a){
+				printf("%d ", x);
+			}
+			puts("");
+			for(const int& x:b){
+				printf("%d ", x);
+			}
+			puts("");
+		}
+	}
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+*/
+
+
+
---
+++ Codeforces/32E - Hide-and-Seek [line intersection, 2300].cpp
@@ -0,0 +1,112 @@
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏ש‏ש‏שבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏ש‏ש‏ש‏¢iש‏שבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏שבשבשבשבשבשבשבש‏שבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏ש‏שבשבש‏¢i¢i¢i¢i¢iשבשבשבשבשבשב¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏שבשבשבשבשבשבש‏¢i¢iשבשבשבשבשבשבש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבשבשבשבש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏¢iש‏שבשבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏ש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשב¢iש‏שבש‏שבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשב¢iש‏שבשבשבשבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏¢iש‏¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢iשבש‏¢iש‏ש‏שבשבשבש‏ש‏¢iש‏שבשבשבשבשבשב¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבש‏ש‏שבש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבש‏¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשב¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢iש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏שבש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢iש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשב¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢iש‏ש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = 1e6 +10;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+C p, v, m1, m2, w1, w2;
+
+double dot(C a, C b){
+	return real(a*conj(b));
+}
+double det(C a, C b){
+	return -imag(a*conj(b));
+}
+C symmetric(C x, C a, C b){
+	return x + 2 * dot(a, b) / dot(b, b) * b - a;
+}
+bool in(C x, C a, C b){
+	return det(a-x, b-x) == 0 && dot(a-x, b-x) <= 0;
+}
+bool intersect(C a, C b, C c, C d){
+	double t1 = det(c-a, b-a) * det(b-a, d-a);
+	double t2 = det(a-c, d-c) * det(d-c, b-c);
+	if(t1<0 || t2<0){
+		return 0;
+	}
+	if(t1==0 && t2==0){
+		return in(a, c, d) || in(b, c, d) || in(c, a, b);
+	}
+	return 1;
+}
+bool check(){
+	if(intersect(p, v, m1, m2)){
+		return det(p-v, m1-m2)==0 && !intersect(p, v, w1, w2);
+	}
+	if(intersect(p, v, w1, w2)){
+		C symP=symmetric(m1, p-m1, m2-m1);
+		C symV=symmetric(m1, v-m1, m2-m1);
+		return intersect(symP, v, m1, m2) && !intersect(symP, v, w1, w2) && !intersect(p, symV, w1, w2);
+	}
+	return 1;
+}
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	double x, y;
+	cin>>x>>y;
+	v=C(x, y);
+	cin>>x>>y;
+	p=C(x, y);
+	cin>>x>>y;
+	w1=C(x, y);
+	cin>>x>>y;
+	w2=C(x, y);
+	cin>>x>>y;
+	m1=C(x, y);
+	cin>>x>>y;
+	m2=C(x, y);
+	puts(check()?"YES":"NO");
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+*/
+
+
+
