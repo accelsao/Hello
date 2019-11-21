09086e06d3cbcb8f0d270ccf1093cde2ef1fa8e7
accelsao <bebe8277@gmail.com>
2019-11-21 21:35:28 +0800

    renames files

--- ATCoder/G-20 B - Ice Rink Game.cpp
+++
@@ -1,9 +0,0 @@
-input()
-L=R=2
-for A in list(map(int,input().split()))[::-1]:
-    L=(L-1)//A*A+A
-    R=R//A*A+A-1
-    if L>R:
-        print(-1)
-        exit()
-print(L,R)
---
+++ Codeforces/1139E - Maximize Mex [bipartite matching, 2400].cpp
@@ -0,0 +1,132 @@
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏ש‏ש‏שבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏ש‏ש‏ש‏¢iש‏שבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבש‏¢i¢i¢iש‏שבשבשבשבשבשבשבש‏שבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏ש‏שבשבש‏¢i¢i¢i¢i¢iשבשבשבשבשבשב¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏שבשבשבשבשבשבש‏¢i¢iשבשבשבשבשבשבש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבשבשבשבש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏¢iש‏שבשבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשב¢i
+// ¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢iש‏ש‏ש‏שבשבשבשבש‏ש‏ש‏ש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏ש‏¢iש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏¢iש‏ש‏ש‏ש‏¢i¢iש‏ש‏ש‏ש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשב¢i¢i
+// ¢i¢i¢i¢iש‏ש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבשב¢iש‏שבש‏שבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏¢i¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשב¢iש‏שבשבשבשבשבש‏¢i¢i
+// ¢i¢i¢i¢i¢iש‏ש‏¢iש‏¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢iשבש‏¢iש‏ש‏שבשבשבש‏ש‏¢iש‏שבשבשבשבשבשב¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏¢i¢i¢i¢i¢i¢i¢iש‏שבשבשבש‏ש‏שבש‏ש‏¢i¢iש‏שבשבשבשבשבשבשבש‏¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏¢i¢iש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשב¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢i¢iש‏ש‏ש‏ש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢iש‏ש‏ש‏שבש‏¢i¢iשבשבשבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏¢iש‏ש‏ש‏ש‏¢i¢i¢iש‏שבשבשבשבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢i¢iש‏ש‏שבשבשבשבשבשבשבשב¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+// ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢iש‏ש‏ש‏¢i¢iש‏ש‏שבשבשבשבשבשבש‏¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i
+#include <bits/stdc++.h>
+using namespace std;
+//#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = 5678;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+
+int n, m, d;
+int p[N];
+int c[N];
+int b[N];
+bool stay[N];
+vector<int> g[N];
+int match[N], ans[N];
+int vis[N];
+int df;
+
+bool dfs(int u){
+	if(vis[u]==df)return 0;
+	vis[u]=df;
+	for(const int& v:g[u]){
+		if(match[v]==-1 || dfs(match[v])){
+			match[v]=u;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>n>>m;
+	for(int i=0;i<n;i++){
+		cin>>p[i];
+		stay[i]=1;
+	}
+	for(int i=0;i<n;i++){
+		cin>>c[i];
+	}
+	cin>>d;
+	for(int i=0;i<d;i++){
+		cin>>b[i];
+		stay[b[i]-1]=0;
+	}
+	
+	RE(match, -1);
+	df=1;
+
+	for(int i=0;i<n;i++){
+		if(stay[i]){
+			g[p[i]].PB(c[i]);
+		}
+	}
+	int ls=0;
+	for(int i=d-1;~i;i--){
+		++df;
+		while(dfs(ls)){
+			df++;
+			ls++;
+		}
+		ans[i]=ls;
+		int u=b[i]-1;
+		g[p[u]].PB(c[u]);
+	}
+	for(int i=0;i<d;i++){
+		printf("%d\n", ans[i]);
+	}
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+
+
+1 = {0} 
+2 = {0, 1, 2}
+3 = {2}  
+
+1 = {0} 
+2 = {0, 1}
+3 = {2}
+
+1 = {0} 
+2 = {0}
+3 = {2}
+*/
+
