c3017dbd70a217cea2f834197ff84606aeb15807
accelsao <bebe8277@gmail.com>
2019-12-16 21:39:07 +0800

    add CF 1276D

---
+++ Codeforces/1276D - Tree Elimination [tree dp, 2600].cpp
@@ -0,0 +1,241 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG
+#define RE(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define SZ(x) ((int)(x.size()))
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using C = complex<double>;
+const double PI = 3.14159265;
+const double eps = 1e-8;
+const int N = 234567;
+const int M = 1e9 + 7;
+const int inf = 1e9+7;
+const ll INF = 1e18;
+ll pw(ll a, ll b){ll t=1;a%=M;for(;b;b>>=1){if(b&1){t=t*a%M;}a=a*a%M;}return t;}
+ll gcd(ll a, ll b){return b?gcd(b, a%b): a;}
+
+template <typename T>
+T inverse(T a, T mod) {
+    T u = 0, v = 1;
+    while (a != 0) {
+        T t = mod / a;
+        mod -= t * a; swap(a, mod);
+        u -= t * v; swap(u, v);
+    }
+    assert(mod == 1);
+    return u;
+}
+
+template <typename T>
+class Modular{
+public:
+    using Type = typename decay<decltype(T::value)>::type;
+
+    constexpr Modular():value(){}
+    template<typename U>
+    Modular(const U& x){
+        value = normalize(x);
+    }
+
+    template <typename U>
+    static Type normalize(const U& x){
+        Type v;
+        if(-mod() <= x && x < mod()) v = static_cast<Type>(x);
+        else v = static_cast<Type>(x % mod());
+        if (v < 0) v += mod();
+        return v;
+    }
+
+    const Type& operator()() const { return value; }
+    template <typename U>
+    explicit operator U() const { return static_cast<U>(value); }
+    constexpr static Type mod() { return T::value; }
+
+    Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+    Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+    template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+    template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+    Modular& operator++() { return *this += 1; }
+    Modular& operator--() { return *this -= 1; }
+    Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+    Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+    Modular operator-() const { return Modular(-value); }
+
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+        #ifdef _WIN32
+        uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+        uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+        asm(
+          "divl %4; \n\t"
+          : "=a" (d), "=d" (m)
+          : "d" (xh), "a" (xl), "r" (mod())
+        );
+        value = m;
+        #else
+        value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+        #endif
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+    int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+        value = normalize(value * rhs.value - q * mod());
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+        value = normalize(value * rhs.value);
+        return *this;
+    }
+    Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+
+    template <typename U>
+    friend const Modular<U>& abs(const Modular<U>& v) { return v; }
+
+    template <typename U>
+    friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
+
+    private:
+        Type value;
+};
+
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+    assert(b >= 0);
+    Modular<T> x = a, res = 1;
+    U p = b;
+    while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+    }
+    return res;
+}
+
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+    return number() == 0;
+}
+
+template <typename T>
+string to_string(const Modular<T>& number) {
+    return to_string(number());
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+    return stream << number();
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+    typename common_type<typename Modular<T>::Type, int64_t>::type x;
+    stream >> x;
+    number.value = Modular<T>::normalize(x);
+    return stream;
+}
+
+constexpr int md = 998244353;
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+Mint dp[N][3];
+// u->v edge, u is v's father
+// dp[0] = remove father
+// dp[1] = remove node
+// dp[2] = tmp ans
+// 1 和 2 的差別在 1 是在 fa and grandfa 被移除前的情況數, 2 是 整個 subtree 都結束的情況
+
+
+vector<vector<int>> g;
+void dfs(int u, int p=-1){
+    dp[u][0]=dp[u][1]=dp[u][2]=0;
+    int flag=0;
+    Mint tmp=1;
+    for(int v: g[u]){
+
+        if(v != p){
+            dfs(v, u);
+            for(int k=0;k<3;k++){
+                dp[u][k] *= dp[v][2];
+            }
+            dp[u][2] += tmp * dp[v][0];
+            if(!flag){
+                dp[u][1] += tmp * dp[v][0];
+            }
+            else {
+                dp[u][0] += tmp * dp[v][0];
+            }
+            tmp = tmp * dp[v][1];
+        }
+        else{
+            flag=1;
+            dp[u][1] += tmp;
+        }
+    }
+    dp[u][0] += tmp;
+    dp[u][2] += tmp;
+}
+
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int n;
+	cin>>n;
+	g.resize(n, vector<int>());
+	int u, v;
+	for(int i=1;i<n;i++){
+        cin>>u>>v;
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+	}
+	dfs(0);
+	cout << dp[0][2] << "\n";
+
+
+}
+
+/*
+#ifdef DEBUG
+	cout<<"I Love Co shu Nie [ https://coshunie.com/ ]"
+#endif
+*/
