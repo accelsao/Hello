e86c3e6b0ea4e152c5040b5112eebca1d4e13a2c
accelsao <bebe8277@gmail.com>
2020-03-27 17:07:41 +0800

    add agdc 043 B

---
+++ ATCoder/AGC 043 B - 123 Triangle.cpp
@@ -0,0 +1,126 @@
+// ███▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓▓╬╬╬╬╬╬▓█
+// ███▓███████▓▓╬╬╬╬╬╬╬╬╬╬╬╬▓███▓▓▓▓█▓╬╬╬▓█
+// ███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+// ████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+// ███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+// ████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+// ███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// █████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// █████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// █████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// ████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// ████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+// █████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+// █████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+// ██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+// ███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+// ███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+// ████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+// █████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+// ██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+// ███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+// ██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+// ███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e6 + 10;
+const int M = 1e9 + 7;
+
+string to_string(string s) {
+  return '"' + s + '"';
+}
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define LOCAL
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+bool lucas(int m, int n){
+    return (m&n)==n;
+}
+
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0);
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	int res=0;
+	bool is_one=false;
+	for(int i=0;i<s.size();i++){
+        int num = s[i]-'1';
+        if(num==1 && lucas(n-1, i)){
+            res ^= 1;
+        }
+        if(num==1){
+            is_one=true;
+        }
+//        debug(k);
+//        k = k * p / q;
+//        p--;
+//        q++;
+	}
+	if(res){
+        cout<<res<<"\n";
+        return 0;
+	}
+    if(is_one){
+        cout<<"0\n";
+        return 0;
+    }
+    res=0;
+//    k=1;
+//    p=n;
+//    q=1;
+    for(int i=0;i<s.size();i++){
+        int num = s[i]-'1';
+        assert(num==0||num==2);
+//        debug(k);
+        if(num==2 && lucas(n-1, i)){
+            res ^= 1;
+        }
+//        k = k * p / q;
+//        p--;
+//        q++;
+	}
+	if(res){
+        cout<<"2\n";
+	} else{
+        cout<<"0\n";
+	}
+}
--- ATCoder/README.md
+++ ATCoder/README.md
@@ -1,3 +1,4 @@
 # Problem
-* ABC157 D - Friend Suggestions / dsu
-* ABC157 E - Simple String Queries / segment tree
+* AGC 043 B - 123 Triangle / `lucas's theorem` `xor`
+* ABC157 D - Friend Suggestions / `dsu`
+* ABC157 E - Simple String Queries / `segment tree`
