9e83c04bcbb1e0e58d0519aea667155fdc46f4e1
accelsao <bebe8277@gmail.com>
2020-07-26 11:52:18 +0800

    add 1338B
    Signed-off-by: accelsao <bebe8277@gmail.com>

---
+++ Codeforces/1338B - Edge Weight Assignment.cpp
@@ -0,0 +1,258 @@
+// ███▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓▓╬╬╬╬╬╬▓█
+// ███▓███████▓▓╬╬╬╬╬╬╬╬╬╬╬╬▓███▓▓▓▓█▓╬╬╬▓█
+// ███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+// ████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+// ███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+// ████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+// ███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// ███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// █████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+// █████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// █████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// ████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+// ████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+// █████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+// █████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+// ██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+// ███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+// ███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+// ████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+// █████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+// ██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+// ███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+// ██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+// ███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template <typename T> using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll myRand(ll B) { return (ull)rng() % B; }
+
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+template <typename A, typename B> string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <typename A> string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define LOCAL
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const ll INF = 2e18;
+const int inf = 1e9 + 7;
+const int N = 1e6 + 10;
+const int M = 1e9 + 7;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+int dw[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+template <typename T> class graph {
+public:
+  struct edge {
+    int from;
+    int to;
+    T cost;
+  };
+
+  vector<edge> edges;
+  vector<vector<int>> g;
+  int n;
+
+  graph(int _n) : n(_n) { g.resize(n); }
+
+  virtual int add(int from, int to, T cost) = 0;
+};
+
+template <typename T>
+class forest : public graph<T> {
+public:
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+
+  forest(int _n) : graph<T>(_n) {}
+
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n);
+    assert(0 <= to && to < n);
+    int id = edges.size();
+    assert(id < n - 1);
+    g[from].push_back(id);
+    g[to].push_back(id);
+    edges.push_back({from, to, cost});
+    return id;
+  }
+};
+
+template <typename T> class dfs_forest : public forest<T> {
+public:
+  using forest<T>::edges;
+  using forest<T>::g;
+  using forest<T>::n;
+
+  vector<int> pv;
+  vector<int> pe;
+  vector<int> order;
+  vector<int> pos;
+  vector<int> end;
+  vector<int> sz;
+  vector<int> root;
+  vector<int> depth;
+  vector<T> dist;
+
+  dfs_forest(int _n) : forest<T>(_n) {}
+
+  void init() {
+    pv = vector<int>(n, -1);
+    pe = vector<int>(n, -1);
+    order.clear();
+    pos = vector<int>(n, -1);
+    end = vector<int>(n, -1);
+    sz = vector<int>(n, 0);
+    root = vector<int>(n, -1);
+    depth = vector<int>(n, -1);
+    dist = vector<T>(n);
+  }
+
+  void clear() {
+    pv.clear();
+    pe.clear();
+    order.clear();
+    pos.clear();
+    end.clear();
+    sz.clear();
+    root.clear();
+    depth.clear();
+    dist.clear();
+  }
+
+private:
+  void do_dfs(int v) {
+    pos[v] = (int)order.size();
+    order.push_back(v);
+    sz[v] = 1;
+    for (int id : g[v]) {
+      if (id == pe[v]) {
+        continue;
+      }
+      auto &e = edges[id];
+      int to = e.from ^ e.to ^ v;
+      depth[to] = depth[v] + 1;
+      dist[to] = dist[v] + e.cost;
+      pv[to] = v;
+      pe[to] = id;
+      root[to] = (root[v] != -1) ? root[v] : to;
+      do_dfs(to);
+      sz[v] += sz[to];
+    }
+    end[v] = (int)order.size() - 1;
+  }
+
+  void do_dfs_from(int v) {
+    depth[v] = 0;
+    dist[v] = T{};
+    root[v] = v;
+    pv[v] = pe[v] = -1;
+    do_dfs(v);
+  }
+
+public:
+  void dfs(int v, bool clear_order = true) {
+    if (pv.empty()) {
+      init();
+    } else {
+      if (clear_order) {
+        order.clear();
+      }
+    }
+    do_dfs_from(v);
+  }
+
+  void dfs_all() {
+    init();
+    for (int v = 0; v < n; v++) {
+      if (depth[v] == -1) {
+        do_dfs_from(v);
+      }
+    }
+    assert((int) order.size() == n);
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(0), cin.tie(0);
+
+  int n;
+  cin>>n;
+  dfs_forest<int> g(n);
+  for(int i=0;i<n-1;i++){
+    int x, y;
+    cin>>x>>y;
+    x--,y--;
+    g.add(x, y);
+  }
+  int root = 0;
+  while(g.g[root].size() < 2) {
+    root ++;
+  }
+
+  g.dfs(root);
+  vector<int> has(2, 0);
+  for(int i=0;i<n;i++){
+    if(g.g[i].size() == 1) {
+      has[g.depth[i] % 2] += 1;
+    }
+  }
+
+  cout << ((has[0] && has[1])? 3 : 1) << " ";
+  set<pii> s;
+  for(auto& e : g.edges) {
+    int x = e.from;
+    int y = e.to;
+    x = ((g.g[x].size() == 1) ? -1 : x);
+    y = ((g.g[y].size() == 1) ? -1 : y);
+    s.emplace(min(x,y), max(x,y));
+  }
+  cout << s.size() << "\n";
+  return 0;
+}
