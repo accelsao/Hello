b2b5205c0abfe078ea61ff8d33a59dcb43cf8b55
accelsao <bebe8277@gmail.com>
2020-07-26 16:07:56 +0800

    add CP template
    Signed-off-by: accelsao <bebe8277@gmail.com>

--- CP Template/A.cpp
+++ CP Template/A.cpp
@@ -27,24 +27,28 @@
 // ███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e6 + 10;
-const int M = 1e9 + 7;
 
-string to_string(string s) {
-  return '"' + s + '"';
-}
-string to_string(const char* s) {
-  return to_string((string) s);
-}
-string to_string(bool b) {
-  return (b ? "true" : "false");
-}
-template <typename A, typename B>
-string to_string(pair<A, B> p) {
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template <typename T> using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll myRand(ll B) { return (ull)rng() % B; }
+
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+template <typename A, typename B> string to_string(pair<A, B> p) {
   return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
 }
-template <typename A>
-string to_string(A v) {
+template <typename A> string to_string(A v) {
   bool first = true;
   string res = "{";
   for (const auto &x : v) {
@@ -58,8 +62,7 @@ string to_string(A v) {
   return res;
 }
 void debug_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {
   cerr << " " << to_string(H);
   debug_out(T...);
 }
@@ -70,6 +73,18 @@ void debug_out(Head H, Tail... T) {
 #define debug(...) 42
 #endif
 
-int main(){
-	ios::sync_with_stdio(0),cin.tie(0);
+const ll INF = 2e18;
+const int inf = 1e9 + 7;
+const int N = 1e6 + 10;
+const int M = 1e9 + 7;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+int main() {
+  ios::sync_with_stdio(0), cin.tie(0);
+  cout << "coshunie daisuki <3 \n";
 }
