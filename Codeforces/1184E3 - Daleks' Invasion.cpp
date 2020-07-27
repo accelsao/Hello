fd8f1255911b8fd6b8a39a761d80e0d387e224c7
accelsao <bebe8277@gmail.com>
2020-07-27 15:18:44 +0800

    add 1184E3
    Signed-off-by: accelsao <bebe8277@gmail.com>

--- Codeforces/1184E3 - Daleks' Invasion.cpp
+++ Codeforces/1184E3 - Daleks' Invasion.cpp
@@ -38,21 +38,6 @@ using pli = pair<ll, int>;
 using pil = pair<int, ll>;
 using pll = pair<ll, ll>;
 
-template<class T> bool set_max(T& a, const T& b) {
-  if(a < b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-template<class T> bool set_min(T& a, const T& b) {
-  if(a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 ll myRand(ll B) { return (ull)rng() % B; }
@@ -81,7 +66,7 @@ template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {
   cerr << " " << to_string(H);
   debug_out(T...);
 }
-#define LOCAL
+//#define LOCAL
 #ifdef LOCAL
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
@@ -99,17 +84,150 @@ const int M = 1e9 + 7;
 #define fi first
 #define se second
 
+int dep[N];
 int p[N];
+int fa[N][20];
+int fb[N][20];
+vector<vector<pii>> con;
 
-int find(int x) {
-  return (x==p[x])? x : p[x] = find(p[x]);
+void dfs(int u) {
+  for (int j = 1; j < 20; j++) {
+    fa[u][j] = fa[fa[u][j - 1]][j - 1];
+  }
+  for (int j = 1; j < 20; j++) {
+    fb[u][j] = max(fb[u][j - 1], fb[fa[u][j - 1]][j - 1]);
+  }
+  for (auto e : con[u]) {
+    int v = e.first;
+    if (v != fa[u][0]) {
+      fb[v][0] = e.second;
+      fa[v][0] = u;
+      dep[v] = dep[u] + 1;
+      dfs(v);
+    }
+  }
 }
+
+int getlca(int x, int y) {
+  if (dep[x] < dep[y]) {
+    swap(x, y);
+  }
+  for (int j = 0; j < 20; j++) {
+    if (((dep[x] - dep[y]) >> j) & 1) {
+      x = fa[x][j];
+    }
+  }
+  if (x == y) {
+    return x;
+  }
+  for (int j = 19; j >= 0; j--) {
+    if (fa[x][j] != fa[y][j]) {
+      x = fa[x][j];
+      y = fa[y][j];
+    }
+  }
+  return fa[x][0];
+}
+
+int getmax(int u, int p) {
+  int ans = 0;
+  for (int j = 19; j >= 0; j--) {
+    if ((dep[u] - dep[p]) >= (1 << j)) {
+      ans = max(ans, fb[u][j]);
+      u = fa[u][j];
+    }
+  }
+  return ans;
+}
+
+void minpth(int u, int p, int v) {
+  for (int j = 19; j >= 0; j--) {
+    if ((dep[u] - dep[p]) >= (1 << j)) {
+      fb[u][j] = min(fb[u][j], v);
+      u = fa[u][j];
+    }
+  }
+}
+
+inline int find(int x) { return (x == p[x]) ? x : p[x] = find(p[x]); }
+
+struct edges {
+  int u, v, w, idx;
+  edges(int u, int v, int w, int idx) : u(u), v(v), w(w), idx(idx) {}
+  bool operator<(const edges &a) const { return w < a.w; }
+};
 
 int main() {
   ios::sync_with_stdio(0), cin.tie(0);
-  int a = inf;
-  set_min(a, 3);
-  cout << a << endl;
-  set_max(a, max({9, 19, 199}));
-  cout << a << endl;
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    p[i] = i;
+  }
+  vector<edges> g;
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    g.push_back(edges(u, v, w, i));
+  }
+  sort(all(g));
+
+  vector<bool> mst(m, false);
+  con.resize(n + 1);
+  for (const auto &e : g) {
+    int x = find(e.u);
+    int y = find(e.v);
+    if (x != y) {
+      p[x] = y;
+      mst[e.idx] = true;
+      con[e.u].emplace_back(e.v, e.w);
+      con[e.v].emplace_back(e.u, e.w);
+    }
+  }
+
+  dep[1] = 1;
+  dfs(1);
+  vector<int> ans(m);
+  for (const auto &e : g) {
+    if (!mst[e.idx]) {
+      int lca = getlca(e.u, e.v);
+      ans[e.idx] = max(getmax(e.u, lca), getmax(e.v, lca));
+    }
+  }
+
+  for (int j = 0; j < 20; j++) {
+    for (int i = 1; i <= n; i++) {
+      fb[i][j] = (int)1e9;
+    }
+  }
+
+  for (const auto &e : g) {
+    if (!mst[e.idx]) {
+      int lca = getlca(e.u, e.v);
+      minpth(e.u, lca, e.w);
+      minpth(e.v, lca, e.w);
+    }
+  }
+
+  for (int j = 19; j > 0; j--) {
+    for (int i = 1; i <= n; i++) {
+      fb[i][j - 1] = min(fb[i][j - 1], fb[i][j]);
+      fb[fa[i][j - 1]][j - 1] = min(fb[fa[i][j - 1]][j - 1], fb[i][j]);
+    }
+  }
+
+  for (auto e : g) {
+    if (mst[e.idx]) {
+      int u = e.u;
+      int v = e.v;
+      if (fa[u][0] == v) {
+        ans[e.idx] = fb[u][0];
+      } else {
+        ans[e.idx] = fb[v][0];
+      }
+    }
+  }
+  for (int i = 0; i < m; i++) {
+    cout << ans[i] << "\n";
+  }
 }
